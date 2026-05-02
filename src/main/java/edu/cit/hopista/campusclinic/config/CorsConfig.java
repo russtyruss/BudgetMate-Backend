112e114b9c6c7b3f65666b22aec70e4c34d0f609
russjie hopista <russjiehopista@russjies-MacBook-Air.local>
2026-05-02 19:54:51 +0800

    fix cors

---
+++ src/main/java/edu/cit/hopista/campusclinic/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package edu.cit.hopista.campusclinic.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                            "http://localhost:3000",
+                            "https://budgetmate-backend-majr.onrender.com"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
